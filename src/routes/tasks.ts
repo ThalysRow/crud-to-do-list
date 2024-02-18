1f303d21f3f49f02f87383cad2002046cd395375
Thalys Row <thalysrogue@gmail.com>
2024-02-18 13:42:45 -0300

    add router delete task

--- src/routes/tasks.ts
+++ src/routes/tasks.ts
@@ -1,17 +1,24 @@
 import { Router } from "express";
 import { validateBody } from "../middlewares/validateBody";
 import { schemaNewTask } from "../utils/joi.schema";
-import { newTask, updateTask } from "../controllers/tasks.Controller";
+import {
+  deleteTask,
+  newTask,
+  updateTask,
+} from "../controllers/tasks.Controller";
 import { validateUpdateTask } from "../middlewares/tasks/validateUpdate";
 
 const taskRouter = Router();
 
 taskRouter.post("/task", validateBody(schemaNewTask), newTask);
+
 taskRouter.patch(
   "/task/:id",
   validateBody(schemaNewTask),
   validateUpdateTask,
   updateTask
 );
+
+taskRouter.delete("/task/:id", validateUpdateTask, deleteTask);
 
 export default taskRouter;
