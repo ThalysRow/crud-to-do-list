5cd3369f9df8e2abe171cc5e8657fb79edef5d5f
Thalys Row <thalysrogue@gmail.com>
2024-02-18 13:06:56 -0300

    add router update task

--- src/routes/tasks.ts
+++ src/routes/tasks.ts
@@ -1,10 +1,17 @@
 import { Router } from "express";
 import { validateBody } from "../middlewares/validateBody";
 import { schemaNewTask } from "../utils/joi.schema";
-import { newTask } from "../controllers/tasks.Controller";
+import { newTask, updateTask } from "../controllers/tasks.Controller";
+import { validateUpdateTask } from "../middlewares/tasks/validateUpdate";
 
 const taskRouter = Router();
 
 taskRouter.post("/task", validateBody(schemaNewTask), newTask);
+taskRouter.patch(
+  "/task/:id",
+  validateBody(schemaNewTask),
+  validateUpdateTask,
+  updateTask
+);
 
 export default taskRouter;
