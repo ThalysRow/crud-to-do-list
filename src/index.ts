08987e0b5b7a2ffaf622bcb790d5ba6105051087
Thalys Row <thalysrogue@gmail.com>
2024-02-18 12:33:05 -0300

    add router tasks

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import "dotenv/config";
 import usersRouter from "./routes/users";
 import { authentication } from "./middlewares/authentication";
+import taskRouter from "./routes/tasks";
 
 const app = express();
 
@@ -10,6 +11,8 @@ app.use(express.json());
 app.use(usersRouter);
 
 app.use(authentication);
+
+app.use(taskRouter);
 
 app.listen(process.env.PORT, () => {
   console.log("Running... 😎");
